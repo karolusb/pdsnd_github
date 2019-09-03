80e8f316b0b90444dd87022a5243bfa204cf89b8
M Charles Baker <karolusbpi@gmail.com>
2019-09-03 15:59:35 -0700

    refactor: remove duplicate list; fix: correct typo
    Found typo in previous refactor and fixed it while making this change.

--- bikesharefin.py
+++ bikesharefin.py
@@ -6,7 +6,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv',
               'c':'chicago.csv',
-              'ny': 'new_york-city.csv',
+              'ny': 'new_york_city.csv',
               'w': 'washington.csv' }
 months = ['january', 'february', 'march', 'april', 'may', 'june']
 days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
@@ -75,7 +75,6 @@ def load_data(city, month, day):
     # filter by month if applicable
     if month != 'all':
         # use the index of the months list to get the corresponding int
-        months = ['january', 'february', 'march', 'april', 'may', 'june']
         month = months.index(month) +1
 
         # filter by month to create the new dataframe
